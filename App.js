0fe508dcf5f3c432f1c6b286d70f7cf62fdfe546
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-15 00:05:49 +0100

    flex basics

--- App.js
+++ App.js
@@ -1,50 +1,32 @@
 // import { StatusBar } from "expo-status-bar"
-import {
- StyleSheet,
- Text,
- View,
- SafeAreaView,
- Button,
- Platform,
- StatusBar,
- Dimensions,
- useWindowDimensions,
-} from "react-native"
-
-// using useWindowsDimensions instead of useDimensions because the latter has been deprecated
-
-import {
- useDimensions,
- useDeviceOrientation,
-} from "@react-native-community/hooks"
+import { View, Text, StyleSheet, ViewBase } from "react-native"
 
 export default function App() {
- const { landscape } = useDeviceOrientation()
- console.log(useDeviceOrientation())
  return (
-  <SafeAreaView style={styles.container}>
+  <View
+   style={{
+    backgroundColor: "#fff",
+    flex: 1,
+   }}
+  >
    <View
     style={{
      backgroundColor: "dodgerblue",
-     width: "100%",
-     height: landscape ? "100%" : "30%",
+     flex: 2,
     }}
-   >
-    <Text></Text>
-   </View>
-  </SafeAreaView>
+   />
+   <View
+    style={{
+     backgroundColor: "gold",
+     flex: 1,
+    }}
+   />
+   <View
+    style={{
+     backgroundColor: "tomato",
+     flex: 1,
+    }}
+   />
+  </View>
  )
 }
-
-const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  backgroundColor: "white",
-  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
- },
- viewStyler: {
-  backgroundColor: "dodgerBlue",
-  width: "50%",
-  height: 70,
- },
-})
