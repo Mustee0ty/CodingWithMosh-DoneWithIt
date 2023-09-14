851106fa0676b59a7f21aca43529421e7c08c8f0
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-14 23:44:53 +0100

    something something something

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar"
+import {
+ StyleSheet,
+ Text,
+ View,
+ SafeAreaView,
+ Button,
+ Platform,
+ StatusBar,
+ Dimensions,
+ useWindowDimensions,
+} from "react-native"
+
+// using useWindowsDimensions instead of useDimensions because the latter has been deprecated
+
+import {
+ useDimensions,
+ useDeviceOrientation,
+} from "@react-native-community/hooks"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ const { landscape } = useDeviceOrientation()
+ console.log(useDeviceOrientation())
+ return (
+  <SafeAreaView style={styles.container}>
+   <View
+    style={{
+     backgroundColor: "dodgerblue",
+     width: "100%",
+     height: landscape ? "100%" : "30%",
+    }}
+   >
+    <Text></Text>
+   </View>
+  </SafeAreaView>
+ )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ container: {
+  flex: 1,
+  backgroundColor: "white",
+  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+ },
+ viewStyler: {
+  backgroundColor: "dodgerBlue",
+  width: "50%",
+  height: 70,
+ },
+})
