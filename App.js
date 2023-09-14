483533330f95a7effbe7843412e87e6c77747c43
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-15 00:41:55 +0100

    Flexbox flexWrap and alignContent

--- App.js
+++ App.js
@@ -9,27 +9,42 @@ export default function App() {
     flex: 1,
     flexDirection: "row",
     justifyContent: "center", // main axis aka horizontal axis
-    alignItems: "center", // secondary axis aka vertical axis
+    alignItems: "center", // secondary axis aka vertical axis in a single line
+    alignContent: "center", // Determines the alignment of items in the whole page aka the whole content, very important when using wrap
+    flexWrap: "wrap",
    }}
   >
    <View
     style={{
      backgroundColor: "dodgerblue",
      width: 100,
-     height: 300,
-     alignSelf: "flex-start",
+     height: 100,
     }}
    />
    <View
     style={{
      backgroundColor: "gold",
      width: 100,
-     height: 200,
+     height: 100,
     }}
    />
    <View
     style={{
      backgroundColor: "tomato",
+     width: 100,
+     height: 100,
+    }}
+   />
+   <View
+    style={{
+     backgroundColor: "grey",
+     width: 100,
+     height: 100,
+    }}
+   />
+   <View
+    style={{
+     backgroundColor: "greenyellow",
      width: 100,
      height: 100,
     }}
