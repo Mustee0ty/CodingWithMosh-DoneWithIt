c21c028ec4502a5676047b5f61cd102d55190cd1
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-16 03:16:38 +0100

    margin and padding

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
-import { View, Text } from "react-native"
+import { View, Text, SafeAreaView } from "react-native"
 import React from "react"
-import WelcomeScreen from "./app/screens/WelcomeScreen"
-import ViewImageScreen from "./app/screens/ViewImageScreen"
+// import WelcomeScreen from "./app/screens/WelcomeScreen"
+// import ViewImageScreen from "./app/screens/ViewImageScreen"
 
 export default function App() {
- return <ViewImageScreen />
+ return (
+  <SafeAreaView>
+   <View
+    style={{
+     justifyContent: "center",
+     alignSelf: "center",
+     backgroundColor: "dodgerblue",
+     width: 100,
+     height: 100,
+     elevation: 60,
+     paddingLeft: 40,
+     marginBottom: 10,
+    }}
+   >
+    <View
+     style={{
+      backgroundColor: "gold",
+      height: 50,
+      width: 50,
+     }}
+    ></View>
+   </View>
+   <View
+    style={{
+     justifyContent: "center",
+     alignSelf: "center",
+     backgroundColor: "red",
+     width: 100,
+     height: 100,
+     elevation: 60,
+     paddingLeft: 40,
+    }}
+   ></View>
+  </SafeAreaView>
+ )
 }
