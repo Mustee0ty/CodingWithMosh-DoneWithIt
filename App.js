9ced4ee0df1888c680dafcee37f2bcbeaab1785f
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-24 23:28:20 +0100

    TextInput

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { View, StyleSheet } from "react-native"
+import { StyleSheet, TextInput, Text } from "react-native"
+import { useState } from "react"
 
-// import AppText
-import AppText from "./app/components/AppText"
-import AppButton from "./app/components/AppButton"
-import WelcomeScreen from "./app/screens/WelcomeScreen"
-import Cards from "./app/screens/Cards"
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
-import ViewImageScreen from "./app/screens/ViewImageScreen"
-import MessagesScreen from "./app/screens/MessagesScreen"
 import Screen from "./app/components/Screen"
-import Icon from "./app/components/Icon"
-import ListItem from "./app/components/ListItem"
-import AccountScreen from "./app/screens/AccountScreen"
-import ListingsScreen from "./app/screens/ListingsScreen"
 
 export default function App() {
- return <ListingsScreen />
+ const [firstName, setFirstName] = useState("")
+
+ return (
+  <Screen>
+   <Text>{firstName}</Text>
+   <TextInput
+    keyboardType="numeric"
+    onChangeText={(text) => setFirstName(text)}
+    placeholder="first name"
+    style={{
+     borderBottomColor: "#ccc",
+     borderBottomWidtht: 1,
+    }}
+   />
+  </Screen>
+ )
 }
 
 const styles = StyleSheet.create({
