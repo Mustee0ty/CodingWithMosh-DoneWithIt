df165fdb4f34b87683fce0731d0efc8e9f2f9afc
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-09-25 02:33:07 +0100

    Building login form button styles not working

--- App.js
+++ App.js
@@ -1,37 +1,8 @@
-import { useState } from "react"
+import React from "react"
+import LoginScreen from "./app/screens/LoginScreen"
 
-import Screen from "./app/components/Screen"
-import AppPicker from "./app/components/AppPicker"
-import AppTextInput from "./app/components/AppTextInput"
-
-const categories = [
- {
-  label: "Furniture",
-  value: 1,
- },
- {
-  label: "Clothing",
-  value: 2,
- },
- {
-  label: "Cameras",
-  value: 3,
- },
-]
-
-export default function App() {
- const [category, setCategory] = useState(categories[0])
-
- return (
-  <Screen>
-   <AppPicker
-    selectedItem={category}
-    onSelectItem={(item) => setCategory(item)}
-    items={categories}
-    icon="apps"
-    placeholder="Cold"
-   />
-   <AppTextInput icon="email" placeholder="Email" />
-  </Screen>
- )
+function App() {
+ return <LoginScreen />
 }
+
+export default App
