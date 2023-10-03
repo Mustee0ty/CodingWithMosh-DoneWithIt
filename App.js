733cca47f026d392de312f33ff7b1e4388a009d1
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-10-03 15:27:15 +0100

    accessing the library using MediaLibrary het Assets Async / Failing

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
 import * as MediaLibrary from "expo-media-library"
+import * as Permissions from "expo-permissions"
 
 import Screen from "./app/components/Screen"
+import { Button, Image, FlatList, View } from "react-native"
 
 function App() {
- //  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions()
+ const [images, setImages] = useState([])
 
  const requestMediaLibraryPermission = async () => {
   const { status } = await MediaLibrary.requestPermissionsAsync()
@@ -17,7 +19,26 @@ function App() {
   requestMediaLibraryPermission()
  }, [])
 
- return <Screen></Screen>
+ const selectImage = async () => {
+  const mediaAssets = await MediaLibrary.getAssetsAsync({ mediaType: "photo" })
+  setImages(mediaAssets.assets)
+ }
+
+ return (
+  <Screen>
+   <Button title="Select Image" onPress={selectImage} />
+   {/* <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} /> */}
+   <FlatList
+    data={images}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item }) => (
+     <View>
+      <Image source={{ uri: item.uri }} style={{ width: 200, height: 200 }} />
+     </View>
+    )}
+   ></FlatList>
+  </Screen>
+ )
 }
 
 export default App
