ccb8e8f1176ca7894fce40b623248269b9e8c8da
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-10-04 16:27:10 +0100

    Accessing image library using imagePicker finally works

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from "react"
 import * as MediaLibrary from "expo-media-library"
+import * as ImagePicker from "expo-image-picker"
 import * as Permissions from "expo-permissions"
 
 import Screen from "./app/components/Screen"
 import { Button, Image, FlatList, View } from "react-native"
 
 function App() {
- const [imageUri, setImageUri] = useState([])
+ const [imageUri, setImageUri] = useState()
 
- const requestMediaLibraryPermission = async () => {
-  const { status } = await MediaLibrary.requestPermissionsAsync()
-  if (status !== "granted") {
+ const requestPermission = async () => {
+  const { granted } = await ImagePicker.requestCameraPermissionsAsync()
+  if (!granted) {
    console.log("Permission to access library denied")
   }
  }
 
  useEffect(() => {
-  requestMediaLibraryPermission()
+  requestPermission()
  }, [])
 
  const selectImage = async () => {
-  const mediaAssets = await MediaLibrary.getAssetsAsync({ mediaType: "photo" })
-  setImageUri(mediaAssets.assets)
+  try {
+   const result = await ImagePicker.launchImageLibraryAsync()
+   if (!result.canceled) setImageUri(result.uri)
+  } catch (error) {
+   console.log("Error reading an image")
+  }
  }
 
  return (
   <Screen>
    <Button title="Select Image" onPress={selectImage} />
-   <Image
-    source={{ uri: setImageUri.uri }}
-    style={{ width: 200, height: 200 }}
-   />
+   <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
   </Screen>
  )
 }
