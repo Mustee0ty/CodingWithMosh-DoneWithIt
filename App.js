5862423cd6243c5727438f3488add53d282a6263
Mustee0ty <Mustaphatasiu55@gmail.com>
2023-10-10 22:30:13 +0100

    stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
-import { Text, View } from "react-native"
-import React, { Component } from "react"
-import ListingEditScreen from "./app/screens/ListingEditScreen"
+import React from "react"
+import { Text, Button } from "react-native"
+import Screen from "./app/components/Screen"
+
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+
+const Tweets = ({ navigation }) => (
+ <Screen>
+  <Text>Tweets</Text>
+  <Button
+   title="View Tweets"
+   onPress={() => navigation.navigate("TweetDetails")}
+  />
+ </Screen>
+)
+
+const TweetDetails = () => (
+ <Screen>
+  <Text>Tweet Details</Text>
+ </Screen>
+)
+
+const Stack = createStackNavigator()
+const StackNavigator = () => (
+ <Stack.Navigator>
+  <Stack.Screen name="Tweets" component={Tweets} />
+  <Stack.Screen name="TweetDetails" component={TweetDetails} />
+ </Stack.Navigator>
+)
 
 export default function App() {
- return <ListingEditScreen />
+ return (
+  <NavigationContainer>
+   <StackNavigator />
+  </NavigationContainer>
+ )
 }
